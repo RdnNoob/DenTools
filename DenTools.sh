c06377531fb1e0c4c2f0b76e6356bf4d6d5a00e3
RdnNoob <52437658+RdnNoob@users.noreply.github.com>
2019-07-02 11:18:21 +0700

    Add files via upload

---
+++ DenTools.sh
@@ -0,0 +1,179 @@
+#usr/bin /sh
+clear
+echo "Tunggu ...."
+sleep 2
+echo "Error Gan Maaf"
+sleep 3
+echo "Tapi Bo'ong :v "
+
+read -p "Lanjut"Enter Goblok
+clear
+sleep 2
+figlet -t  "RadenTools"
+echo "============================================"
+echo "Masukkan Username Dan Password"
+echo "Tanya 4ng3lS/DenS Kalau Tidak Tau"
+echo " Hub : 082223361183 / Gmail : raden.muhammad.ichsan@gmail.com "
+
+sleep 1
+
+echo "Masukkan Username"
+
+read -p  "Username:",..$ bro =
+
+if [ $bro = RadenGans ] || [ $bro = RadenGans ]
+then
+
+echo "Masukkan Password"
+
+read -p "Password:",...$ bro =
+
+if [ $bro = Nolep ] || [ $bro = Nolep ]
+then
+sleep 1
+echo "        PLEASE WAIT"
+sleep 1
+echo "============="
+sleep 2
+echo "==========================="
+sleep 3
+echo "======================================="
+sleep 2
+echo "======================================="
+sleep 1
+echo "======================================="
+clear
+sleep 4
+echo
+echo " OK SukSes.."
+sleep 2
+clear
+fi
+fi
+
+read -p "Lanjut?",ENTER GOBLOK
+clear
+sleep 1
+cd Data
+figlet -t "RadenTools"
+sleep 2
+echo "========================================"
+echo "#                                      #"
+echo "# Raden Tools                          #"
+echo "# Author: 4ng3lS/DenS                  #"
+echo "# Version : 1.0                        #"
+echo "# Supported : Mr.C3ndol & P4TR1CK      #"
+echo "# Thanks for Team ; SF|NOOB PEOPLE     #"
+echo "# And Thank To my Love : Dwi Listiyani #"
+echo "#                                      #"
+echo "========================================"
+sleep 1
+echo
+echo "Bila Ada Error/Kesalahan Pada Tools Harap Hubungi 4ng3lS/DenS 08222336113 . "
+echo
+sleep 2
+read -p "Enter Tod",Enter Goblok 
+sleep 1.5
+echo "================================================"
+echo "      Menu Tools             "
+echo "                             "
+sleep 1
+echo "   B.Install Bahan              "
+sleep 1
+echo "  01.Malicious (buat bikin Virus)  "
+sleep 1
+echo "  02.Link Bokep (ngeBokep Biar Hidup gk sia sia) "
+sleep 1
+echo "  03.termux-ohmyzsh (buat memperindah Termux Lu Asw)          "
+sleep 1
+echo "  04.Spamer Campuran (yg ini Recode ea :v)      "
+sleep 1
+echo "  05.Tools SETAN Pashayogi (buat Hack FB Bujank) "
+sleep 1
+echo "  06.Kun-Tool (Tools Installer by C3ndol)"
+sleep 1
+echo " 00.EXIT TOOLS       "
+echo "================================================"
+echo "                                          "
+sleep 1
+echo "               GUNAKAN DENGAN BIJAK ASU!!"
+sleep 1
+read -p  "Pilih Angkanya Tolol...="$ bro =
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+cd malicious
+pip2 install requests
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+figlet "4ng3lS" 
+echo "noh tinggal copas ya asw"
+echo "www.xnxx.com"
+echo "www.xhamters.com"
+echo "www.xjilbab.xyz"
+echo "nekopoi.net"
+echo "pornhub.com"
+echo "E.Exit"
+read -p $ bro=
+fi
+if [ $bro = E ] || [ $bro = e ]
+then
+cd DenTools
+sh DenTools.sh
+fi
+
+if [ $bro = 3 ] || [ $bro= 03 ]
+then
+clear
+figlet "4ng3lS" 
+read -p "RUN",Enter
+cd termux-ohmyzsh
+sh install.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 04]
+then
+clear
+cd SPAMER
+sh Spamer.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 05 ]
+then
+cd SETAN
+pip install requests
+pip2 install requests
+pip2 install mechanize
+python2 SETAN.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 06 ]
+then
+cd Kun-Tool
+sh Kun-Tool.sh
+fi
+
+if [ $bro = B ] || [ $bro = b ]
+then
+sh bahan.sh
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+clear
+sleep 1
+echo "Good By"
+sleep 1
+echo "See You Next Time"
+sleep 1
+echo "Loe Semua Buriq Kontoll"
+sleep 1
+exit
+fi
+
